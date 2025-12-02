cdd179fcef8280619402fa3bbf47483be50ca800
Yury Verpatov <inaunius@gmail.com>
2025-12-02 10:31:45 +0600

    Analyzer fixed

--- src/analyzer.py
+++ src/analyzer.py
@@ -6,6 +6,14 @@ from deeppavlov import build_model, configs
 from constant_configs import *
 from gigachat import GigaChat
 
+DATA_TYPE = "DATE"
+ORG_TYPE = "ORG"
+LOC_TYPE = "LOC"
+LAW_TYPES = ("LAW", "NORMA", "MISC")
+
+UNKNOWN_TAG = "O"
+BEGIN_TAG_PREFIX = "B-"
+IN_TAG_PREFIX = "I-"
 
 deep_pavlov_model = build_model(
 	configs.ner.ner_rus_bert, 
@@ -13,23 +21,24 @@ deep_pavlov_model = build_model(
 	install=True
 )
 
-
 def _ner_extract(text: str) -> List[Dict]:
 	entities = []
 	current = None
 
-	# for token in deep_pavlov_model([text]):
-	# 	print(token, end="\n\n")
 	tokens, tags = deep_pavlov_model([text])
-
 	for token, tag in zip(tokens[0], tags[0]):
-		if tag.startswith("B-"):
+		if tag == UNKNOWN_TAG:
+			continue
+		
+		if tag.startswith(BEGIN_TAG_PREFIX):
 			if current:
 				entities.append(current)
-				current = {"type": tag[2:], "text": token}
-			elif tag.startswith("I-") and current:
-				current["text"] += " " + token
-			else:
+			current = { "type": tag[2:], "text": token }
+		
+		elif tag.startswith(IN_TAG_PREFIX) and current:
+			current["text"] += " " + token
+		
+		else:
 				if current:
 					entities.append(current)
 					current = None
@@ -42,82 +51,24 @@ def _ner_extract(text: str) -> List[Dict]:
 def _extract_key_changes(description: str, title: str) -> Dict:
 	text = f"{title}. {description}"
 	entities = _ner_extract(text)
-	dates = [e["text"] for e in entities if e["type"] == "DATE"]
-	orgs = [e["text"] for e in entities if e["type"] == "ORG"]
-	laws = [e["text"] for e in entities if e["type"] in ("LAW", "NORMA", "MISC")]
+
+	dates = [entity["text"] for entity in entities if entity["type"] == DATA_TYPE]
+	orgs = [entity["text"] for entity in entities if entity["type"] == ORG_TYPE]
+	locs = [entity["text"] for entity in entities if entity["type"] == LOC_TYPE]
+	laws = [entity["text"] for entity in entities if entity["type"] in LAW_TYPES]
 
 	return {
 		"dates": list(set(dates)),
 		"orgs": list(set(orgs)),
 		"laws": list(set(laws)),
+		"locs": list(set(locs)),
 		"raw_entities": entities
 	}
 
+def _do_test_dump(changes: list) -> None:
+	with open("test_dump.json", "w", encoding="utf-8") as dump_file:
+		json.dump(changes, dump_file, indent=2)
 
-
-
-
-# def _ask_yandex_gpt(prompt: str) -> str:
-# 	headers = {
-# 		"Content-Type": "application/json",
-# 		"Authorization": f"Api-Key {YANDEX_API_KEY}"
-# 	}
-# 	payload = {
-# 		"modelUri": "gpt://foundationModels/yandexgpt/latest",
-# 		"completionOptions": {"maxTokens": 500, "temperature": 0.2},
-# 		"messages": [{"role": "user", "text": prompt}]
-# 	}
-
-# 	resp = requests.post(YANDEX_URL, headers=headers, json=payload)
-# 	resp.raise_for_status()
-
-# 	return resp.json()["result"]["alternatives"][0]["message"]["text"]
-
-# def _ask_gigachat(prompt: str) -> str:
-# 	# headers = {
-# 	#     "Authorization": f"Bearer {GIGACHAT_TOKEN}",
-# 	#     "Content-Type": "application/json"
-# 	# }
-# 	# payload = {
-# 	#     "model": "GigaChat",
-# 	#     "messages": [{"role": "user", "content": prompt}],
-# 	#     "temperature": 0.2,
-# 	#     "max_tokens": 500
-# 	# }
-# 	# resp = requests.post(GIGACHAT_URL, headers=headers, json=payload)
-# 	# resp.raise_for_status()
-		
-# 	with GigaChat(credentials=GIGACHAT_TOKEN, verify_ssl_certs=False) as giga:
-# 		return giga.chat(prompt).json()["choices"][0]["message"]["content"]
-
-# def _generate_report(changes: List[Dict]) -> str:
-# 	formatted = []
-# 	for ch in changes:
-# 		formatted.append(
-# 			f"""
-# Название: {ch["title"]}
-# URL: {ch["url"]}
-# Выделенные даты: {", ".join(ch["analysis"]["dates"]) or "-"}
-# Нормативные акты: {", ".join(ch["analysis"]["laws"]) or "-"}
-# Организации: {", ".join(ch["analysis"]["orgs"]) or "-"}
-# """
-# )
-# 	prompt = (
-# 		"Сформируй аналитический отчёт о законодательных изменениях.\n"
-# 		"Структура:\n"
-# 		" - краткое содержание изменений\n"
-# 		" - ключевые даты\n"
-# 		" - упомянутые нормативные акты\n"
-# 		" - значимость изменений\n\n"
-# 		"Данные:\n" +
-# 		"\n".join(formatted)
-# 	)
-
-# 	try:
-# 		return _ask_yandex_gpt(prompt)
-# 	except:
-# 		return _ask_gigachat(prompt)
-		
 def analyze_all():
 	connection = sqlite3.connect(DB_PATH)
 	cursor = connection.cursor()
@@ -134,12 +85,6 @@ def analyze_all():
 			"description": description,
 			"analysis": analysis
 		})
-		print(f"[OK] ANALYZED: {title}")
+		print(f"ANALYZED: {title}")
 
-	print(changes)
-	# print("\GENERATING REPORT:\n")
-	# report = _generate_report(changes)
-	# with open("law_report.txt", "w", encoding="utf-8") as f:
-	# 	f.write(report)
-	# print("REPORT GENERATED")
-
+	_do_test_dump(changes)
